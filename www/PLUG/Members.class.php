plugorgau/ugmm

commit a9cd2503e04b7fcf0f431fd180d42dd28cdb5bae

added createTimestamp and modifyTimestamp to values returned by load_ldap, for determining how long ago pending members signed up

www/PLUG/Members.class.php
@@ -545,7 +545,11 @@ class Person {
             'pager',
             'description',
             'shadowExpire',
-            'memberOf'));
+            'memberOf',
+            'createTimestamp',
+            'modifyTimestamp' ));
+
+        ));
         if (PEAR::isError($this->ldapentry)) {
             throw new Exception('LDAP Error: load_ldap: '.$this->ldapentry->getMessage());
         }

commit 1f3e6fb66baf01e3e5a1b1d20061134cc45f62b8

Fix up syntax error introduced in a9cd250

www/PLUG/Members.class.php
@@ -547,8 +547,7 @@ class Person {
             'shadowExpire',
             'memberOf',
             'createTimestamp',
-            'modifyTimestamp' ));
-
+            'modifyTimestamp'
         ));
         if (PEAR::isError($this->ldapentry)) {
             throw new Exception('LDAP Error: load_ldap: '.$this->ldapentry->getMessage());