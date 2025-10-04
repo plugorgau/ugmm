88eeefc83359f05cef417e09095b4106765c21ee
James Henstridge <james@jamesh.id.au>
2025-10-04 17:47:38 +0800

    config: define configuration constants with const rather than define

--- www/PLUG/config.inc.php
+++ www/PLUG/config.inc.php
@@ -4,20 +4,20 @@
  * Define all things here that are Organisation specific
  */
 
-define('CONCESSION_AMOUNT', 2000);
-define('FULL_AMOUNT', 5000);
+const CONCESSION_AMOUNT = 2000;
+const FULL_AMOUNT = 5000;
 
-define('CONCESSION_TYPE', 2);
-define('FULL_TYPE', 1);
+const CONCESSION_TYPE = 2;
+const FULL_TYPE = 1;
 
-define('COMMITTEE_EMAIL', "committee@plug.org.au");
-define('CONTACT_EMAIL', "committee@plug.org.au");
-define('WEBMASTERS_EMAIL', "webmasters@plug.org.au");
-define('ADMIN_EMAIL', "admin@plug.org.au");
-define('SCRIPTS_FROM_EMAIL', "PLUG Membership Scripts <admin@plug.org.au>");
-define('SCRIPTS_REPLYTO_EMAIL', "PLUG Committee <committee@plug.org.au>");
+const COMMITTEE_EMAIL = "committee@plug.org.au";
+const CONTACT_EMAIL = "committee@plug.org.au";
+const WEBMASTERS_EMAIL = "webmasters@plug.org.au";
+const ADMIN_EMAIL = "admin@plug.org.au";
+const SCRIPTS_FROM_EMAIL = "PLUG Membership Scripts <admin@plug.org.au>";
+const SCRIPTS_REPLYTO_EMAIL = "PLUG Committee <committee@plug.org.au>";
 
-define('PAYMENT_OPTIONS',
+const PAYMENT_OPTIONS =
     " (a) Head down to the next PLUG workshop or seminar to pay your dues to
      a committee member (e-mail ".COMMITTEE_EMAIL." beforehand to make
      sure there will be somebody there to renew your membership).
@@ -26,8 +26,7 @@ define('PAYMENT_OPTIONS',
      http://www.plug.org.au/membership for details), and email
      ".COMMITTEE_EMAIL." to let them know you have deposited it.
      Credit card facilities are available if no other method is
-     available to you, just contact the committee to organise."
-);
+     available to you, just contact the committee to organise.";
 
-define('LDAP_BASE', 'dc=plug,dc=org,dc=au');
-define('DEFAULT_MEMBER', 'cn=admin,dc=plug,dc=org,dc=au');
+const LDAP_BASE = 'dc=plug,dc=org,dc=au';
+const DEFAULT_MEMBER = 'cn=admin,dc=plug,dc=org,dc=au';
