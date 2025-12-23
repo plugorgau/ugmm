d7675b12eadebe7a6d52d7e3141b6c4475730072
James Henstridge <james@jamesh.id.au>
2025-12-23 14:38:48 +0800

    Move exmaples directory to the toplevel, and include the ldif files used by the CI.

--- ci/setup-slapd.sh
+++ ci/setup-slapd.sh
@@ -3,6 +3,7 @@
 set -ex
 
 curdir="$(dirname "$0")"
+examplesdir="$curdir/../examples/ldap"
 
 echo $curdir
 
@@ -15,7 +16,7 @@ EOF
 apt-get install -y slapd ldap-utils
 
 # Replace nis schema with rfc2307bis
-cp $curdir/rfc2307bis.ldif /etc/ldap/schema/
+cp $examplesdir/rfc2307bis.ldif /etc/ldap/schema/
 sed -i 's/nis\.ldif/rfc2307bis.ldif/' /usr/share/slapd/slapd.init.ldif
 
 # Now use set up the database with the appropriate domain and password
@@ -31,16 +32,16 @@ EOF
 dpkg-reconfigure slapd
 
 # Load and enable the memberof plugin
-ldapadd -H ldapi:// -Y EXTERNAL -f $curdir/load-memberof.ldif
+ldapadd -H ldapi:// -Y EXTERNAL -f $examplesdir/load-memberof.ldif
 
 # Add UGMM's plugpen schema
 ldapadd -H ldapi:// -Y EXTERNAL -f /etc/ldap/schema/namedobject.ldif
 ldapadd -H ldapi:// -Y EXTERNAL -f $curdir/../plugpen.ldif
 
 # Load basic structure
-ldapadd -H ldapi:// -D cn=admin,dc=plug,dc=org,dc=au -w plug -f $curdir/basic-structure.ldif
-ldapadd -H ldapi:// -D cn=admin,dc=plug,dc=org,dc=au -w plug -f $curdir/specialgroups.ldif
+ldapadd -H ldapi:// -D cn=admin,dc=plug,dc=org,dc=au -w plug -f $examplesdir/basic-structure.ldif
+ldapadd -H ldapi:// -D cn=admin,dc=plug,dc=org,dc=au -w plug -f $examplesdir/specialgroups.ldif
 
 # Add example users
-ldapadd -H ldapi:// -D cn=admin,dc=plug,dc=org,dc=au -w plug -f $curdir/user-chair.ldif
-ldapadd -H ldapi:// -D cn=admin,dc=plug,dc=org,dc=au -w plug -f $curdir/user-bob.ldif
+ldapadd -H ldapi:// -D cn=admin,dc=plug,dc=org,dc=au -w plug -f $examplesdir/user-chair.ldif
+ldapadd -H ldapi:// -D cn=admin,dc=plug,dc=org,dc=au -w plug -f $examplesdir/user-bob.ldif
